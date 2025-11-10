a83605ab3148fc3fba27ccebee04bffc3281c95a
Hrushikesh Nandi <hrushikesh1200@gmail.com>
2025-11-10 22:11:11 +0530

    Mood tracker app assignment

---
+++ mood_change/mood_tracker/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MoodComponent from "./MoodComponent";
+
+function App() {
+  return (
+    <div>
+      <MoodComponent name="Hrushikesh" />
+    </div>
+  );
+}
+
+export default App;
---
+++ mood_change/mood_tracker/src/MoodComponent.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "./MoodComponent.css";
+
+function MoodComponent({ name }) {
+  // All moods in an array
+  const moods = ["Happy 😃", "Sad 😞", "Excited 🤩", "Angry 😠", "Relaxed 😌"];
+
+  // Track current mood index
+  const [moodIndex, setMoodIndex] = useState(0);
+
+  // Function to switch to the next mood
+  const toggleMood = () => {
+    setMoodIndex((prevIndex) => (prevIndex + 1) % moods.length);
+  };
+
+  return (
+    <div className="mood-container">
+      <h2 className="mood-title">Hello, {name}! 👋</h2>
+      <h3 className="mood-text">
+        Current Mood: <span>{moods[moodIndex]}</span>
+      </h3>
+      <button onClick={toggleMood} className="mood-btn">
+        Change Mood
+      </button>
+    </div>
+  );
+}
+
+export default MoodComponent;
