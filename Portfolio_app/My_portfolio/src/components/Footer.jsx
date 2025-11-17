2aaa9c4f3d6fb8dd750698bac2100ba40f3177a3
Hrushikesh Nandi <hrushikesh1200@gmail.com>
2025-11-17 09:36:13 +0530

    Delete file

--- Portfolio_app/My_portfolio/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© {new Date().getFullYear()} MyPortfolio. All Rights Reserved.</p>
-    </footer>
-  );
-}
